f48620ce2c601c309176635000c73987de905bf0
NsebaN <nuwanabeysooriya@gmail.com>
2018-11-21 00:10:24 +0530

    with photo upload functions

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 
@@ -42,9 +43,21 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        User::create($request->all());
+        $input = $request->all();
+        if($file = $request->file('photo_id')){
+            
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            
+            $photo = Photo::create(['file'=> $name]);
+            $input['photo_id'] = $photo->id;
+            
+        }
         
-        return view('admin.users');
+        $input['password'] = bcrypt($request->password);
+        User::create($input);
+        
+        return view('admin.users.index');
     }
 
     /**
