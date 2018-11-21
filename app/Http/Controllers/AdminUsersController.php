adea4db6e74be0fa7a6658715c3a5d63a7b2c305
NsebaN <nuwanabeysooriya@gmail.com>
2018-11-21 13:34:21 +0530

    before Admin security upgrade

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UserEditRequest;
 use App\Photo;
 use App\Role;
 use App\User;
@@ -30,8 +31,7 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        $roles = Role::lists('name','id')->all();         
-//        return $roles; 
+        $roles = Role::lists('name','id')->all(); 
         return view('admin.users.create', compact('roles'));
     }
 
@@ -43,7 +43,13 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        $input = $request->all();
+        if(trim($request->password) == ''){
+            $input = $request->except('password');            
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+        
         if($file = $request->file('photo_id')){
             
             $name = time() . $file->getClientOriginalName();
@@ -54,10 +60,9 @@ class AdminUsersController extends Controller
             
         }
         
-        $input['password'] = bcrypt($request->password);
         User::create($input);
         
-        return view('admin.users.index');
+        return redirect('/admin/users');
     }
 
     /**
@@ -79,7 +84,10 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit');
+        $user  = User::findOrFail($id);
+        $roles = Role::lists('name', 'id')->all();
+        
+        return view('admin.users.edit', compact('user','roles'));
     }
 
     /**
@@ -89,9 +97,27 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
-        //
+        $user  = User::findOrFail($id);
+        if(trim($request->password) == ''){
+            $input = $request->except('password');            
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+        
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+        
+        $user->update($input);
+        
+        return redirect('/admin/users');
     }
 
     /**
